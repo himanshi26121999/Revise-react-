964294b00e5cb26ebb110328252f7622e4e577c3
himanshi26121999 <abhayrajc55@gmail.com>
2025-07-04 12:53:22 +0530

    Managing users list

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./AddUser.css";
 
-const AddUser = () => {
+const AddUser = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
@@ -19,7 +19,7 @@ const AddUser = () => {
     if(enteredName.trim().length === 0 || enteredAge.trim().length === 0 || enteredAge<=0){
       return;
     }
-    console.log(enteredName,enteredAge);
+    props.onAddUser(enteredName,enteredAge);
     setEnteredName(""); setEnteredAge("");
   };
 
